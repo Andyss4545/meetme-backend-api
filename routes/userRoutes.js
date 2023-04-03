2ef6be0fb4b4ab9bb66b8e2c5755287b7e51c4b0
GentleAndy <andystemidwebguru@gmail.com>
2023-04-03 07:29:00 -0700

    removed the auth routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const { getUser, deleteUser, updateUser, followUser, unfollowUser } = require('../controllers/userController');
 const router = express.Router();
 
 
-router.put('/login', (req, res) => {
-     res.status(201).json({message: "This is login"})
-})
 
+router.get('/:id', getUser)
+router.delete('/:id', deleteUser)
+router.put('/:id', updateUser)
+router.put('/follow/:id', followUser)
+router.put('/unfollow/:id', unfollowUser)
 
 
 module.exports = router
